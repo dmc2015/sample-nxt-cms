2a9fd1162737fe84b807af51aaa6839d6c3e8325
Don McLamb <mclamb.donald@gmail.com>
2024-10-05 14:28:19 -0500

    debugging

---
+++ public/admin/index.js
@@ -0,0 +1,11 @@
+import CMS from "decap-cms-app"; // Import the CMS
+// Import your custom template if you have one
+// import MyTemplate from './path/to/template';
+
+// Initialize the CMS object
+CMS.init();
+
+// Optionally register a preview template
+// CMS.registerPreviewTemplate("my-template", MyTemplate);
+
+// You can also register other features or components here if needed
