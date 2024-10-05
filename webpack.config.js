f879a8049a4c5540464c2ad5f282bc19a8d8a3e8
Don McLamb <mclamb.donald@gmail.com>
2024-10-05 14:57:44 -0500

    debug webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,24 @@
-// webpack.config.js
 const path = require('path');
 
 module.exports = {
-  watch: true,
-  entry: './public/admin/index.js', // The input file (your CMS entry point)
+  entry: './public/admin/index.js',
   output: {
-    filename: 'cms.js', // The output file name
-    path: path.resolve(__dirname, 'public/admin'), // Output directory
+    path: path.resolve(__dirname, 'public/admin'),
+    filename: 'cms.js',
   },
-  mode: 'production', // Use production mode for minified output
   module: {
     rules: [
       {
-        test: /\.js$/, // Use babel-loader for JavaScript files
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
       },
     ],
   },
-  resolve: {
-    alias: {
-      // If decap-cms-app needs an alias, you can add it here
-      'decap-cms-app': path.resolve(__dirname, 'node_modules/decap-cms-app'),
-    },
-  },
+  mode: 'development', // or 'production'
 };
